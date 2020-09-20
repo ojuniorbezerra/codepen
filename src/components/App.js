a2ff4d7cb33a7fc6d57ff6e276a9573c9de11de4
ojuniorbezerra <junio_vjc@hotmail.com>
2020-09-19 21:57:28 -0300

    set editor for html, js and css

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Editor from  './Editor'
 
 function App() {
+const [html, setHtml, css, setCss, js, setJs] = useState('')
+
   return (
     <>
-    <div className="pane top-pane">
-      <Editor/>
-      <Editor/>
-      <Editor/>
-
-    </div>
-    <div className="pane"></div>
-    <iframe
-    title="output"
-    sandbox="allow-scripts"
-    frameBorder="0"
-    width="100%"
-    height="100%"
-    />
+      <div className="pane top-pane">
+        <Editor language="xml" displayName="HTML" value={html} onChange={setHtml}/>
+        <Editor language="css" displayName="CSS" value={css} onChange={setCss}/>
+        <Editor language="javascript" displayName="js" value={js} onChange={setJs}/>
+      </div>
+      <div className="pane"/>
+      <iframe
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+      />
     </>
   );
 }
