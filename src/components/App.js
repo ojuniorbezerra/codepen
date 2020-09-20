90263943bb309bf1522ba10d0564abbdeec3626a
ojuniorbezerra <junio_vjc@hotmail.com>
2020-09-19 21:32:35 -0300

    create component Editor

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Editor from  './Editor'
 
 function App() {
-  return ("Hello Wold"
+  return (
+    <>
+    <div className="pane top-pane">
+      <Editor/>
+      <Editor/>
+      <Editor/>
+
+    </div>
+    <div className="pane"></div>
+    <iframe
+    title="output"
+    sandbox="allow-scripts"
+    frameBorder="0"
+    width="100%"
+    height="100%"
+    />
+    </>
   );
 }
 
