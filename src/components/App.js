e8f29ea9eeecb6480f291b7cc1016812f074436f
ojuniorbezerra <junio_vjc@hotmail.com>
2020-09-20 12:04:03 -0300

    start implemantation editor

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Editor from  './Editor'
 
 function App() {
-const [html, setHtml, css, setCss, js, setJs] = useState('')
+const [html, setHtml] = useState('')
+const [css, setCss] = useState('')
+const [js, setJs] = useState('')
+const [srcDoc, setSrcDoc] = useState('')
 
+useEffect(() => {
+  const timeout = setTimeout(() => {
+    setSrcDoc(`
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>
+    `)
+  }, 250)
+
+  return () => clearTimeout(timeout)
+}, [html, css, js])
   return (
     <>
       <div className="pane top-pane">
         <Editor language="xml" displayName="HTML" value={html} onChange={setHtml}/>
         <Editor language="css" displayName="CSS" value={css} onChange={setCss}/>
-        <Editor language="javascript" displayName="js" value={js} onChange={setJs}/>
+        <Editor language="javascript" displayName="JS" value={js} onChange={setJs}/>
       </div>
       <div className="pane"/>
       <iframe
+        srcDoc={srcDoc}
         title="output"
         sandbox="allow-scripts"
         frameBorder="0"
